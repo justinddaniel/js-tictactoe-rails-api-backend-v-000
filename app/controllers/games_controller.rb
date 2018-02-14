80560ae6006b85a2edc80b6bf8ccab50d24e4eaf
Justin Daniel <justinddaniel@gmail.com>
2018-02-14 00:24:42 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,30 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
 
+  def index
+    @games = Games.all
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.create(game_params)
+    render json: @game, status: 201
+  end
+
+  def show 
+    @game = Game.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @game }
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
 end
